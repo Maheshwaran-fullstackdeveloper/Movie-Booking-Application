cc65093a5ff03df8c052fca294e9bbfa7cfc867c
Maheshwaran G <maheshwaran30g@gmail.com>
2026-03-26 12:29:49 +0530

    patch fixed function arguments error

--- server/inngest/index.js
+++ server/inngest/index.js
@@ -8,19 +8,19 @@ export const inngest = new Inngest({ id: "movie-ticket-booking" });
 const syncUserCreation = inngest.createFunction(
   {
     id: "sync-user-from-clerk",
-  },
-  {
-    event: "clerk/user.created",
+    triggers: [{ event: "clerk/user.created" }],
   },
   async ({ event }) => {
     const { id, first_name, last_name, email_addresses, image_url } =
       event.data;
+
     const userData = {
       _id: id,
       email: email_addresses[0].email_address,
       name: `${first_name} ${last_name}`,
       image: image_url,
     };
+
     await User.create(userData);
   },
 );
@@ -29,13 +29,11 @@ const syncUserCreation = inngest.createFunction(
 const syncUserDeletion = inngest.createFunction(
   {
     id: "delete-user-with-clerk",
-  },
-  {
-    event: "clerk/user.deleted",
+    triggers: [{ event: "clerk/user.deleted" }],
   },
   async ({ event }) => {
     const { id } = event.data;
-    await User.findByIdAndDelete({ _id: id });
+    await User.findByIdAndDelete(id);
   },
 );
 
@@ -43,20 +41,19 @@ const syncUserDeletion = inngest.createFunction(
 const syncUserUpdation = inngest.createFunction(
   {
     id: "update-user-from-clerk",
-  },
-  {
-    event: "clerk/user.updated",
+    triggers: [{ event: "clerk/user.updated" }],
   },
   async ({ event }) => {
     const { id, first_name, last_name, email_addresses, image_url } =
       event.data;
+
     const userData = {
-      _id: id,
       email: email_addresses[0].email_address,
       name: `${first_name} ${last_name}`,
       image: image_url,
     };
-    await User.findByIdAndUpdate({ _id: id }, userData);
+
+    await User.findByIdAndUpdate(id, userData);
   },
 );
 
--- server/server.js
+++ server/server.js
@@ -4,13 +4,16 @@ import "dotenv/config";
 import connectDB from "./configs/db.js";
 import { clerkMiddleware } from "@clerk/express";
 import { serve } from "inngest/express";
-import { inngest, functions } from "./src/inngest/index.js";
+import { inngest, functions } from "./inngest/index.js";
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 await connectDB();
+
 app.use(express.json());
+
 app.use(clerkMiddleware());
+
 app.use(cors());
 
 app.get("/", (req, res) => {
