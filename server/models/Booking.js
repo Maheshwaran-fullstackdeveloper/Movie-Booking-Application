25b86aeb31c5e6cb6f1f19d689acded3168efa93
Maheshwaran G <maheshwaran30g@gmail.com>
2026-03-29 21:34:23 +0530

    Implemented features before Booking model

---
+++ server/models/Booking.js
@@ -0,0 +1,37 @@
+import mongoose from "mongoose";
+
+const bookingSchema = new mongoose.Schema(
+  {
+    user: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    movie: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Movie",
+      required: true,
+    },
+    show: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Show",
+      required: true,
+    },
+    seats: {
+      type: [String],
+      required: true,
+    },
+    totalPrice: {
+      type: Number,
+      required: true,
+    },
+    status: {
+      type: String,
+      enum: ["pending", "success", "failed"],
+      default: "pending",
+    },
+  },
+  { timestamps: true },
+);
+
+export default mongoose.model("Booking", bookingSchema);
